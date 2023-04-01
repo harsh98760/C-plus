b6bcc38e531f576f35aebb010c3bd44fb24b6e27
Harsh Kumar Rana <59533607+harsh98760@users.noreply.github.com>
2023-04-01 15:18:07 +0530

    Add files via upload

---
+++ dailycodingpractice.cpp
@@ -0,0 +1,102 @@
+// PROBLEM 1:-- ake value of length and bredth of rectangle from user and check if it is square or not?
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int length , breadth;
+//     cout<<"Please Enter The Length of the rectangle"<<endl;
+//     cin>>length;
+//     cout<<"Please Enter The Breadth of the rectangle"<<endl;
+//     cin>>breadth;
+
+//     if(length==breadth){
+//         cout<<"It is sqaure"<<endl;
+//     }
+//     else{
+//         cout<<"It is rectangle"<<endl;
+//     }
+
+//     return 0;
+// }
+
+
+//PROBLEM 2:-- Take two int values from user and print greatest among them.
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a , b; // two values for int
+//     cout<<"Enter your first value: "<<endl;
+//     cin>>a;
+//     cout<<"Enter your second value: "<<endl;
+//     cin>>b;
+//     if(a==b){
+//         cout<<"Both are equal";
+//     }
+//     else if(a>b){
+//         cout<<"A is greather than B";
+//     }
+//     else{
+//         cout<<"B is grather than A"<<endl;
+//     }
+//     return 0;
+// }
+
+
+/*A shop will give discount of 10% if the cost of purchased quantity is more than 1000
+Ask user for quantity 
+Suppose, one unit will cost 100
+Judge and print total cost for user*/
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int quantity, price;
+//     cout<<"Enter Quantity"<<endl;
+//     cin>>quantity;
+//     price= quantity*100;
+//     if(price>1000){
+//         cout<<"Total cost is "<<price-(price*10/100)<<endl;
+//     }
+//     else{
+//         cout<<"Total cost is "<<price<<endl;
+//     }
+//     return 0;
+// }
+
+
+                         /*Problem 3 
+A company decided to give bonus of 5$ to employess if his/her year of service is more than 5 years.
+Ask user for their salary and year of service and print the net bonus amount
+*/
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int salary , yos, bonus;
+//     cout<<"Please Enter your salary:  "<<endl;
+//     cin>>salary;
+//     cout<<"Enter your giving valuable year of service: "<<endl;
+//     cin>>yos;
+//     if(yos>5){
+//         cout<<"Your net bonus amount: "<<salary*5/100<<endl;
+//     }
+//     else{
+//         cout<<"You are not eligible of the bonus and your monthly salary is : "<<salary<<endl;
+//     }
+//     return 0;
+// }
+
+
+/*                           Problem 4
+A school has following rules for garding system:
+Belew 25 -> F
+25 to 45 -> E
+45 to 50 -> D
+50 to 60 -> C
+Above 80 -> A
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    return 0;
+}
